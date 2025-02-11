d8a7ea6b074abb3544f822b99b67445744052f1e
Daniel Falster <daniel.falster@unsw.edu.au>
2025-02-11 13:40:05 +0000

    version 1.1.3

--- tests/testthat/test-versions.R
+++ tests/testthat/test-versions.R
@@ -1,10 +1,10 @@
 test_that("Retrieval is possible", {
   skip_on_ci()
+  skip_on_cran()
   
   Sys.setenv("NETWORK_UP" = TRUE)
   versions <- get_versions()
   
   expect_visible(versions)
-  expect_named(versions)
 })
 
